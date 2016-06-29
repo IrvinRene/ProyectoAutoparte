6a65ad10de7c4e62206f9ec607d8b61744d35782
McNava <mc.allen.navarro@gmail.com>
2016-06-29 18:55:09 -0500

    Controlador y vista evaluacion

---
+++ resources/views/Evaluacion.blade.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Evaluacion</title>
+
+		
+	</head>
+	<body>
+		
+				<div class="title">Evaluacion</div><br>
+				<div class="quote">¿Como se siente usted utilizando este sitio?</div>
+				<input type="radio" name="cuestion1" value="1">Muy Satisfecho<br>
+				<input type="radio" name="cuestion1" value="2">Satisfecho<br>
+				<input type="radio" name="cuestion1" value="3">Insatisfecho<br>
+				<input type="radio" name="cuestion1" value="4">Muy Insatisfecho<br>
+				<div class="quote">¿Tiene usted algun comentario sobre la pagina?</div>
+				<textarea name="comentario" cols="50" rows="5" wrap="soft"></textarea><br>
+				<input name="Enviar" value="Enviar" type="submit">
+		
+	</body>
+</html>
