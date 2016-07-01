9595d158487cc13ce493656a8a23f34515e9329e
lukita24 <a_gonzalezc24@hotmail.com>
2016-07-01 08:39:43 -0700

    Inventario

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,7 @@ Route::get('consulta', 'ConsultaController@index'); //LIBNY
 Route::resource('marca', 'MarcaController');//IRVIN
 Route::resource('pieza', 'PiezaController');//Sandra
 Route::resource('modelo', 'ModeloController');// Alex
+Route::resource('inventario', 'InventarioController');//Lukita
 //Anais esta parte creo que te sirve a ti
 Route::get('home', 'HomeController@index');
 Route::controllers([
