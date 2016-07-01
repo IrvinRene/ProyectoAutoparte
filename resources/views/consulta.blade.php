c61cd7d90dfd68e637d911e138fc00e1fdff23ff
Libny Patiño <simeyli16@hotmail.com>
2016-06-30 22:36:05 -0500

    Consulta
    Adaptada al estilo de Jarly, cosas estéticas los cambios....

--- resources/views/consulta.blade.php
+++ resources/views/consulta.blade.php
@@ -1,18 +1,18 @@
 
-@extends('app')
+@extends('layouts.principal')
 
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-sm-12 col-m-d-offset-1">
-			<div class="panel panel-default">
+		<div class="col-sm-5 col-m-d-offset 5">
+			<div class="panel panel-success">
 				<div class="panel-heading bg-success">Consulta</div>
 
 				<div class="panel-body">
 					{{--<div class="row">--}}
 						¿Qué estás buscando? Ingresa tus datos ... <br>
 						{{--{!!Form::model($consulta,$parametros['ruta'])!!}--}}
-						<form>
+						<form clas="form-horizontal">
 									
 							<div class="form-group{{ $errors->has('idrol') ? ' has-error' : '' }}">
 								<label class="col-sm-3 control-label">Marca</label>
@@ -36,21 +36,28 @@
 							</div>
 
 							<div class="form-group{{ $errors->has('idrol') ? ' has-error' : '' }}">
-								<label class="col-sm-3 control-label">Motor</label>
+								<label class="col-sm-3 control-label">Año</label>
 								<div class="col-sm-7">
 									{!! Form::select('id', App\Agnos::lists('agno', 'id'), null, array('class' => 'form-control')) !!}
 								</div>
 							</div>
 
 							<div class="form-group{{ $errors->has('idrol') ? ' has-error' : '' }}">
-								<label class="col-sm-3 control-label">Motor</label>
+								<label class="col-sm-3 control-label">Pieza</label>
 								<div class="col-sm-7">
 									{!! Form::select('id', App\Pieza::lists('pieza', 'id'), null, array('class' => 'form-control')) !!}
 								</div>
+								<br></br>
 							</div>
 
 						<br><br>
-						<input class="Buscar" value="Buscar" type="submit">
+						<div clas="form-group">
+							<br><br>
+							<div class="col-sm-offset-4 col-sm-6">
+								<button class="bnt bnt-success" type="submit"> Buscar </button>
+							</div>
+						</div>
+					</form>
 						{!!Form::close()!!}
 					{{--</div>--}}
 				</div>
