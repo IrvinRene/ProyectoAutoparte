ed846da02b0cdf520f74128e892b95412694755e
IrvinRene <r26.irvinrene@gmail.com>
2016-07-02 23:21:57 -0500

    actualizacion para anais

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,8 +22,8 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers;
 
-    protected $redirectTo = '/inicio';
-    protected $redirectAfterLogout = 'inicio';
+    protected $redirectTo = '/';
+    protected $redirectAfterLogout = '/';
 
     /**
      * Create a new authentication controller instance.
