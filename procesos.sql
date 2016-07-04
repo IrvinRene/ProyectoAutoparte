7d68d28935798ba3d180c713dc6239fd34b8c115
lukita24 <a_gonzalezc24@hotmail.com>
2016-07-03 20:13:09 -0700

    tabla procesos

---
+++ procesos.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE IF NOT EXISTS `procesos` (
+  `id` int(11) NOT NULL,
+  `proceso` varchar(7) NOT NULL,
+  `created_at` varchar(45) NOT NULL,
+  `updated_at` varchar(45) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1; 
+
+INSERT INTO `procesos` (`id`, `proceso`, `created_at`, `updated_at`) VALUES
+(1, 'Entrada', '', ''),
+(2, 'Salida', '', '');
+
+ ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `procesos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
